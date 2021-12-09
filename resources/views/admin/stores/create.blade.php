8600b60223c520440af3951c325aa87da5feaee6
Thalyson Luiz <thalysonluiz@gmail.com>
2021-12-09 18:31:48 -0300

    Recuperar dados digitados no form

--- resources/views/admin/stores/create.blade.php
+++ resources/views/admin/stores/create.blade.php
@@ -5,21 +5,21 @@
         <input type="hidden" name="_token" value="{{csrf_token()}}">
     <div class="form-group">
         <label>Nome Loja:</label>
-        <input type="text" name="name" id="" class="form-control @error('name') is-invalid @enderror">
+        <input type="text" name="name" id="" class="form-control @error('name') is-invalid @enderror" value="{{old('name')}}">
         @error('name')
             <div class="invalid-feedback">{{$message}}</div>
         @enderror
     </div>
     <div class="form-group">
         <label>Descrição</label>
-        <input type="text" name="description" id="" class="form-control @error('description') is-invalid @enderror">
+        <input type="text" name="description" id="" class="form-control @error('description') is-invalid @enderror" value="{{old('description')}}">
         @error('description')
             <div class="invalid-feedback">{{$message}}</div>
         @enderror
     </div>
     <div class="form-group">
         <label>Telefone</label>
-        <input type="text" name="phone" id="" class="form-control @error('phone') is-invalid @enderror">
+        <input type="text" name="phone" id="" class="form-control @error('phone') is-invalid @enderror" value="{{old('phone')}}">
         @error('phone')
             <div class="invalid-feedback">{{$message}}</div>
         @enderror
